fd40ca1396d31810d262c6c440e8118ca52fbb22
Florencia <florenciag251198@gmail.com>
2024-03-15 14:47:58 -0300

    Added only one round To test if it works.

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
+let computerSelection = getComputerChoice();
+let userSelection = prompt("Choose!");
+
+
+function firstRound(userSelection, computerSelection){
+    let firstLetterUser = (userSelection.slice(0,1)).toUpperCase();
+    let restOfLetters = (userSelection.slice(1,userSelection.length)).toLowerCase();
+    let userSelection2 = firstLetterUser+restOfLetters;
+    if (userSelection2===computerSelection){
+        return "Tie! Try again."
+    }
+    else if (userSelection2=="Rock" && computerSelection=="Paper"){
+        return "Paper beats Rock! You lose!";
+    }
+    else if (userSelection=="Rock" && computerSelection=="Scissors"){
+        return "Rock beats Scissors! You Win!";
+    }
+    else if (userSelection=="Paper" && computerSelection=="Rock"){
+        return "Paper beats Rock! You Win!";
+    }
+    else if (userSelection=="Paper" && computerSelection=="Scissors"){
+        return "Scissors beats Paper! You lose!";
+    }
+    else if (userSelection=="Scissors" && computerSelection=="Rock"){
+        return "Rock beats Scissors! You lose!";
+    }
+    else{
+        return "Scissors beats Paper! You Win!";
+    }
+}
+
+
 function getComputerChoice(){
     const choiceOptions=["Rock","Paper","Scissors"];
     let numberChosen=Math.floor(Math.random()*choiceOptions.length);
-    console.log(numberChosen);
     return choiceOptions[numberChosen];
 }
 
-console.log(getComputerChoice());
+console.log("Computer selection: "+ computerSelection);
+console.log("User selection: "+ userSelection);
+console.log(firstRound(userSelection,computerSelection));
